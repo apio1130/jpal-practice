66da9697f0da14ffefc0acc41c8c679d6608b069
apio1130 <apio1130@naver.com>
2021-02-10 17:29:32 +0900

    feat(페이징): JPQL의 페이징 쿼리 요청
    아래와 같이 두가지의 페이징 정보를 추가해주는 것으로 페이징 처리
    setFirstResult 시작 idx
    setMaxResults 마지막 idx

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -18,53 +18,32 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            // Member member = new Member();
+            // member.setUsername("member1");
+            // member.setAge(10);
+            // em.persist(member);
+
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member" + (i+1));
+                member.setAge(i + 1);
+                em.persist(member);
+            }
 
             em.flush();
             em.clear();
 
-            // 프로젝션
-            // 1) 엔티티 프로젝션
-            // List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
-            // Member findMember = result.get(0);
-            // findMember.setAge(20);  // 20으로 변경되어 저장됨. 조회 결과가 영속성 컨텐스트에 전부 관리됨.
+            // 페이징
+            List<Member> members = em.createQuery("select m from Member m order by m.age desc ", Member.class)
+                .setFirstResult(1)
+                .setMaxResults(10)
+                .getResultList();
 
-            // 2) 인티티 프로젝션
-            // List<Team> result = em.createQuery("select m.team from Member m", Team.class).getResultList();
-            //  -> inner join Query로 조회함(묵시적 조인). 조인의 사용은 성능에 영향을 많이 받기에 아래와 같이 명시적 조인으로 작성
-            // List<Team> result = em.createQuery("select t from Member m join m.team t", Team.class).getResultList();
-            // inner join Query로 조회함(명시적 조인). jpql이 join을 사용한다고 직관적으로 알 수 있음.
+            System.out.println("result.size = " + members.size());
+            for(Member member1 : members) {
+                System.out.println("member1 = " + member1);
+            }
 
-            // 3) 임베디드 타입 프로젝션
-            // em.createQuery("select o.address from Order o", Address.class).getResultList();
-            // 임베디드 타입을 조회하기 위해서는 소속된 Entity에서 조회를 시작해줘야한다.
-            // from Address로 실행 불가능
-
-            // 4) 스칼라 타입 프로젝션
-            // 4-1) Query 타입 조회
-            // List resultList = em.createQuery("select m.username, m.age from Member m").getResultList();
-            // Object o = resultList.get(0);
-            // Object[] result = (Object[])o;
-            // System.out.println("username = " + result[0]);
-            // System.out.println("age = " + result[1]);
-
-            // 4-2) Object[] 타입으로 조회
-            // List<Object[]> resultList = em.createQuery("select m.username, m.age from Member m").getResultList();
-            // Object[] result = resultList.get(0);
-            // System.out.println("username = " + result[0]);
-            // System.out.println("age = " + result[1]);
-
-            // 4-3) new 명령어로 조회
-            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class).getResultList();
-            MemberDTO memberDTO = resultList.get(0);
-            System.out.println("memberDTO = " + memberDTO.getUsername());
-            System.out.println("memberDTO = " + memberDTO.getAge());
-            // 단순 값을 조회하기에는 new DTO 명령어로 조회하는 방법을 추천.
-            // 패키지 명을 포함한 전체 클래스명을 입력
-            // 순서와 타입이 일치하는 생성자 필요
 
             tx.commit();
         } catch (Exception e) {
